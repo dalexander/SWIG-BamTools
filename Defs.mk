0711c37888faa6f92edb9cffd2be478f2671f7a3
David Alexander <dalexander@pacificbiosciences.com>
2014-07-15 12:01:47 -0700

    Comment out Mac build stuff

--- Defs.mk
+++ Defs.mk
@@ -1,7 +1,10 @@
 ROOT_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
+GXX ?= g++
+# Mac peope may prefer this:
+#GXX ?= clang++ -stdlib=libc++
 
-CXX := g++ -fPIC -g
+CXX := $(GXX) -fPIC -g
 BAMTOOLS_INCLUDE := $(ROOT_DIR)/bamtools/include
 BAMTOOLS_LIB := $(ROOT_DIR)/bamtools/lib
 SWIG_INTERFACE := $(ROOT_DIR)/BamTools.i
